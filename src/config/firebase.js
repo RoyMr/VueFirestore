ec2738564fa9d80edfc17dd4bd57d7eab21e1791
RoyMr <cursodeseguridadit@gmail.com>
2018-05-28 14:11:24 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,3 @@
 export default {
-    apiKey: "AIzaSyDzT_-DjALED9QHu13pzzoOj5aYPrdSPmI",
-    authDomain: "cursovuefirestore-f88d4.firebaseapp.com",
-    databaseURL: "https://cursovuefirestore-f88d4.firebaseio.com",
-    projectId: "cursovuefirestore-f88d4",
-    storageBucket: "cursovuefirestore-f88d4.appspot.com",
-    messagingSenderId: "768185044652"
+
 };
